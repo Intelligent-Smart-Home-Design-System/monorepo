fe4ea8d65b03e931333b185b0d7fdef1c8b7e1b9
Andrew Tarkhanov <amtarkhanov@edu.hse.ru>
2026-01-22 18:55:21 +0300

    implemented a fire in the existing infrastructure

--- services/simulation/internal/entities/actors/incidents.go
+++ services/simulation/internal/entities/actors/incidents.go
@@ -2,58 +2,80 @@ package actors
 
 // Пожар, протечка и тд
 
-// ЗАГОТОВКА:
-//const (
-//	N = 5
-//	M = 5
-//	FireDistributionTime = 1.0
-//)
-//
-//type Cell struct {
-//	x, y  int
-//	burnt bool
-//}
-//
-//func fireProcess(proc simgo.Process, grid [][]*Cell, cell *Cell) {
-//	if cell.burnt {
-//		return
-//	}
-//
-//	cell.burnt = true
-//	fmt.Printf("Time %.1f: fire at (%d, %d)\n", proc.Now(), cell.x, cell.y)
-//
-//	proc.Wait(proc.Timeout(FireDistributionTime))
-//
-//	directions := [][2]int{{1, 0}, {-1, 0}, {0, 1}, {0, -1}}
-//
-//	for _, d := range directions {
-//		newx, newy := cell.x + d[0], cell.y + d[1]
-//		if newx >= 0 && newx < N && newy >= 0 && newy < M {
-//			neighbor := grid[newx][newy]
-//			if !neighbor.burnt {
-//				proc.Process(func(p simgo.Process) {
-//					fireProcess(p, grid, neighbor)
-//				})
-//			}
-//		}
-//	}
-//}
-//
-//func main() {
-//	sim := simgo.NewSimulation()
-//
-//	grid := make([][]*Cell, N)
-//	for i := 0; i < N; i++ {
-//		grid[i] = make([]*Cell, M)
-//		for j := 0; j < M; j++ {
-//			grid[i][j] = &Cell{x: i, y: j}
-//		}
-//	}
-//
-//	start := grid[2][2]
-//	sim.Process(func(proc simgo.Process) {
-//		fireProcess(proc, grid, start)
-//	})
-//
-//	sim.Run()
-//}
+import (
+	"fmt"
+
+	"github.com/Intelligent-Smart-Home-Design-System/monorepo/services/simulation/internal/field"
+	"github.com/fschuetz04/simgo"
+)
+
+type Fire struct {
+	id         string
+	field      field.GeneralField
+	startCell  *field.Cell
+	trigger    *simgo.Event
+	spreadTime float64
+}
+
+func NewFire(id string, field field.GeneralField, start *field.Cell, spreadTime float64) *Fire {
+	return &Fire{
+		id:         id,
+		field:      field,
+		startCell:  start,
+		spreadTime: spreadTime,
+	}
+}
+
+// ////////////////////////////////
+// реализация интерфейса Entity
+func (f *Fire) SetEvent() {
+}
+
+func (f *Fire) Trigger(delay int) float64 {
+	return 0
+}
+
+func (fire *Fire) Process(process simgo.Process) {
+	for {
+		if fire.trigger.Triggered() {
+			fire.trigger = process.Simulation.Event()
+		}
+
+		process.Wait(fire.trigger)
+
+		fmt.Printf(
+			"Fire started at time %.1f in (%d,%d)",
+			process.Simulation.Now(),
+			fire.startCell.X,
+			fire.startCell.Y,
+		)
+
+		fire.burn(process, fire.startCell)
+	}
+}
+
+//////////////////////////////////
+
+func (fire *Fire) burn(proc simgo.Process, cell *field.Cell) {
+	if cell.Burnt {
+		return
+	}
+	cell.Burnt = true
+	fmt.Printf(
+		"Fire at (%d,%d), time %.1f",
+		cell.X,
+		cell.Y,
+		proc.Simulation.Now(),
+	)
+
+	proc.Wait(proc.Timeout(fire.spreadTime))
+
+	for _, n := range fire.field.GetNeighbors(cell) {
+		if !n.Burnt {
+			neighbor := n
+			proc.Process(func(p simgo.Process) {
+				fire.burn(p, neighbor)
+			})
+		}
+	}
+}

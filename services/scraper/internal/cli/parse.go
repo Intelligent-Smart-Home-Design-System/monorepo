a058ed28ffedf96dd1e154d141425531aa05ad8a
pl553 <110288373+pl553@users.noreply.github.com>
2026-04-13 13:50:41 +0300

    add scrape/parse jobs for scraper; Dockerfile

---
+++ services/scraper/internal/cli/parse.go
@@ -0,0 +1,38 @@
+package cli
+
+import (
+	"context"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/rs/zerolog"
+	"github.com/spf13/cobra"
+)
+
+func NewParseCmd() *cobra.Command {
+	var cfgFile string
+
+	cmd := &cobra.Command{
+		Use:   "parse",
+		Short: "Run the parsing job",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			ctx, cancel := signal.NotifyContext(cmd.Context(), os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+			defer cancel()
+
+			return parse(ctx, cfgFile)
+		},
+	}
+
+	cmd.Flags().StringVar(&cfgFile, "config", "./config.toml", "config file")
+
+	return cmd
+}
+
+func parse(ctx context.Context, cfgFile string) error {
+	logger := zerolog.New(os.Stderr).With().Timestamp().Logger()
+
+	logger.Info().Msg("Hello World")
+
+	return nil
+}
